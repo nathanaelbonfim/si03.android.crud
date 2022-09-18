60bbdfc3cf062e05b72f87b8cadf01d982c190f1
Yohann Gonçalves <36939172+BlackburnRX@users.noreply.github.com>
2022-09-17 21:46:19 -0300

    ADD cadastro de fornecedor

---
+++ app/src/main/java/com/si03/android/bim3/crud/ProviderForm.java
@@ -0,0 +1,14 @@
+package com.si03.android.bim3.crud;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProviderForm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_provider_form);
+    }
+}
