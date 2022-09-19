866aec106de553097a295d802208ec99575204cd
Yohann Gonçalves <36939172+BlackburnRX@users.noreply.github.com>
2022-09-18 21:37:32 -0300

    ADD chamada entre as activities

--- app/src/main/java/com/si03/android/bim3/crud/ProviderForm.java
+++ app/src/main/java/com/si03/android/bim3/crud/ProviderForm.java
@@ -2,7 +2,9 @@ package com.si03.android.bim3.crud;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ProviderForm extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class ProviderForm extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_provider_form);
+    }
+    public void cadastroFornecedor(View view){
+
+        Intent intent = new Intent(this, Providers.class);
+        startActivity(intent);
     }
 }
